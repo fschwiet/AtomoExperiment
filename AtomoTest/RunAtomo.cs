535327376c7e09e6d8d4942f1a278aba76de398e
fschwiet <fschwiet@gmail.com>
2011-07-16 17:28:06 -0700

    this is not the way

---
+++ AtomoTest/RunAtomo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+
+namespace AtomoTest
+{
+    public class RunAtomo : GivenWhenThenFixture
+    {
+        public override void Specify()
+        {
+            //  http://sueetie.com/wiki/UsingAtomo.ashx
+
+
+            // given the atomo zip has been decompressed to the target directory
+            // build the top-level solution
+            // configure IIS sites
+            // drop/create database
+            // update database connection string
+            // update WCF endpoints
+
+            throw new NotImplementedException();
+        }
+    }
+}
