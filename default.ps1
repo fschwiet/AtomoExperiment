ecf213862195be65c4f9f13257231db797b796bd
fschwiet <fschwiet@gmail.com>
2011-07-16 17:50:56 -0700

    adding unzip task

--- default.ps1
+++ default.ps1
@@ -1,12 +1,33 @@
 
 # http://sueetie.com/wiki/UsingAtomo.ashx
 
+properties {
+	$atomoZip = "C:\inetpub\Sueetie.Atomo.3.2.0.zip"
+	$siteTarget = "c:\inetpub\Sueetie.Atomo.Test"
+}
+
 task default -depends TestDeploy
 
-task CleanupDeploymentDirectory { }
+task CleanupIISConfig {}
+
+task CleanupDeploymentDirectory { 
+
+	if (test-path $siteTarget) {
+		$null = rm $siteTarget -recurse -force
+	}
+}
+
+
 task CleanupDeploymentDatabase { }
-task Cleanup -depends CleanupDeploymentDirectory, CleanupDeploymentDatabase {}
-task UnzipAtomo {}
+task Cleanup -depends CleanupIISConfig, CleanupDeploymentDirectory, CleanupDeploymentDatabase {}
+
+task UnzipAtomo {
+
+	"Unzipping atomo to $siteTarget..." | write-host
+	$null = exec {  & ".\tools\7-Zip\7za.exe" x $atomoZip "-o$siteTarget" }
+}
+
+
 task BuildAtomo {}
 task CreateAtomoInIIS {}
 task ConfigureAtomo {
